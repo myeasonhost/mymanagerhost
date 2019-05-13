c4acf02600c7613c484248ce243b4bdc8f89accd
suk <5426903>
2019-05-13 14:10:33 +0800

    新增vo

--- report-api/service-report-pull-ag/src/main/java/com/eason/report/pull/ag/ICommonAPI.java
+++ report-api/service-report-pull-ag/src/main/java/com/eason/report/pull/ag/ICommonAPI.java
@@ -1,8 +1,6 @@
 package com.eason.report.pull.ag;
 
-import com.eason.report.pull.ag.vo.common.SumordersDaysVo;
-import com.eason.report.pull.ag.vo.common.SumordersDaysXinVo;
-import com.eason.report.pull.ag.vo.common.SumordersVo;
+import com.eason.report.pull.ag.vo.common.*;
 
 /**
  * @apiDefine common 通用API
@@ -26,5 +24,25 @@ public interface ICommonAPI {
 
     public String getsumorders_days(SumordersDaysVo vo);
 
-    public String getsumorders_days_xin(SumordersDaysXinVo vo);
+    public String getsumorders_daysgetsumorders_days(SumordersDaysXinVo vo);
+
+    public String getOrders(OrdersVo vo);
+
+    public String getSlotOrdersEX(SlotOrdersVo vo);
+
+    public String getYoPlayOrdersEX (YoPlayOrdersEX vo);
+
+    public String getSlotOrdersTEX (SlotOrdersTEX vo);
+
+    public String getAgSportOrdersEX(AgSportOrdersEX vo);
+
+    public String getXinSlotEventRes(XinSlotEventRes vo);
+
+    public String getXinEventRes(XinEventRes vo);
+
+    public String getComporders(Comporders vo);
+
+    public String getGameOrders (GameOrders vo);
+
+    public String getScenesOfUserReportExt (ScenesOfUserReportExt vo);
 }
---
+++ report-api/service-report-pull-ag/src/main/java/com/eason/report/pull/ag/vo/common/OrdersVo.java
@@ -0,0 +1,42 @@
+package com.eason.report.pull.ag.vo.common;
+
+import java.io.Serializable;
+
+public class OrdersVo implements Serializable {
+    private String cAgent;
+    private String startDate;
+    private String endDate;
+    private String key;
+
+    public String getcAgent() {
+        return cAgent;
+    }
+
+    public void setcAgent(String cAgent) {
+        this.cAgent = cAgent;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
