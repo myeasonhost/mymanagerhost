873628e4a0860dc6bb9b84daaa8d2459bd45cb8c
EASON LI <mdt006@mdtgaming.com>
2019-05-13 17:32:36 +0800

    项目初始化

--- report-api-impl/service-report-pull-ag-impl/src/main/java/com/eason/report/pull/ag/common/CommonAPIImpl.java
+++ report-api-impl/service-report-pull-ag-impl/src/main/java/com/eason/report/pull/ag/common/CommonAPIImpl.java
@@ -1,9 +1,7 @@
 package com.eason.report.pull.ag.common;
 
 import com.eason.report.pull.ag.ICommonAPI;
-import com.eason.report.pull.ag.vo.common.SumordersDaysVo;
-import com.eason.report.pull.ag.vo.common.SumordersDaysXinVo;
-import com.eason.report.pull.ag.vo.common.SumordersVo;
+import com.eason.report.pull.ag.vo.common.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +29,57 @@ public class CommonAPIImpl implements ICommonAPI {
     }
 
     @Override
-    public String getsumorders_days_xin(SumordersDaysXinVo vo) {
+    public String getsumorders_daysgetsumorders_days(SumordersDaysXinVo vo) {
+        return null;
+    }
+
+    @Override
+    public String getOrders(OrdersVo vo) {
+        return null;
+    }
+
+    @Override
+    public String getSlotOrdersEX(SlotOrdersVo vo) {
+        return null;
+    }
+
+    @Override
+    public String getYoPlayOrdersEX(YoPlayOrdersEX vo) {
+        return null;
+    }
+
+    @Override
+    public String getSlotOrdersTEX(SlotOrdersTEX vo) {
+        return null;
+    }
+
+    @Override
+    public String getAgSportOrdersEX(AgSportOrdersEX vo) {
+        return null;
+    }
+
+    @Override
+    public String getXinSlotEventRes(XinSlotEventRes vo) {
+        return null;
+    }
+
+    @Override
+    public String getXinEventRes(XinEventRes vo) {
+        return null;
+    }
+
+    @Override
+    public String getComporders(Comporders vo) {
+        return null;
+    }
+
+    @Override
+    public String getGameOrders(GameOrders vo) {
+        return null;
+    }
+
+    @Override
+    public String getScenesOfUserReportExt(ScenesOfUserReportExt vo) {
         return null;
     }
 }
