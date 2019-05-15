829923e0cb53bda08b63b7695af0f6a1e35b9f84
EASON LI <mdt006@mdtgaming.com>
2019-05-15 15:45:41 +0800

    项目初始化

--- report-api-impl/service-report-pull-ag-impl/src/main/java/com/eason/report/pull/ag/api/CommonAPIImpl.java
+++ report-api-impl/service-report-pull-ag-impl/src/main/java/com/eason/report/pull/ag/api/CommonAPIImpl.java
@@ -45,11 +45,6 @@ public class CommonAPIImpl extends BaseAPI implements ICommonAPI {
     }
 
     @Override
-    public String getsumorders_daysgetsumorders_days(SumordersDaysXinVo vo) {
-        return null;
-    }
-
-    @Override
     public String getOrders(OrdersVo vo) {
         return null;
     }
