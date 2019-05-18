bb071258a2d29d69e4da602a5eec9101c92028a7
ares <493807042@qq.com>
2019-05-18 15:53:32 +0800

    添加pt项目

---
+++ report-api-impl/service-report-pull-pt-impl/src/test/java/com/eason/report/pull/pt/ServiceReportPullPtImplApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eason.report.pull.pt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceReportPullPtImplApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
