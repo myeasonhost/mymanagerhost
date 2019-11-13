56143a404a9b0815ea70bdf8c7210c4cf4975817
eason <eason123456>
2019-11-13 16:21:13 +0800

    架构设计

--- transfer-api-impl/service-transfer-api-user/src/main/java/com/eason/transfer/openapi/user/api/UserServiceImpl.java
+++ transfer-api-impl/service-transfer-api-user/src/main/java/com/eason/transfer/openapi/user/api/UserServiceImpl.java
@@ -1,17 +1,17 @@
 package com.eason.transfer.openapi.user.api;
 
-import com.eason.transfer.openapi.user.api.model.DemoRequest;
-import com.eason.transfer.openapi.user.api.model.DemoResponse;
+import com.eason.transfer.openapi.core.sdk.user.DemoRequest;
+import com.eason.transfer.openapi.core.sdk.user.DemoResponse;
+import com.eason.transfer.openapi.core.sdk.user.DemoServiceImpl;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController("userServiceImpl")
-public class UserServiceImpl {
+public class UserServiceImpl implements DemoServiceImpl {
 
     @RequestMapping(value = "/test")
-    public DemoResponse test(Map<String,String> request) {
+    public DemoResponse test(@RequestBody DemoRequest request) {
         System.out.println(request);
         DemoResponse response=new DemoResponse();
         response.setResult("demo返回结果2222222222222222");
