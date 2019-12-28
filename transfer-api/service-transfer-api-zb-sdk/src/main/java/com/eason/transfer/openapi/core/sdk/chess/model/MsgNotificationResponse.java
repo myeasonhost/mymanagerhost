e45f9a868030a3f26835527f898017ef6e5a8904
ares123456 <ares123@qq.com>
2019-12-28 16:51:35 +0800

    zb接口

---
+++ transfer-api/service-transfer-api-zb-sdk/src/main/java/com/eason/transfer/openapi/core/sdk/chess/model/MsgNotificationResponse.java
@@ -0,0 +1,20 @@
+package com.eason.transfer.openapi.core.sdk.chess.model;
+
+import com.eason.transfer.openapi.core.common.response.Response;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@XStreamAlias("MsgNotificationResponse")
+public class MsgNotificationResponse extends Response {
+    private static final long serialVersionUID = 1L;
+
+    private List<MsgNotificationResponseVo> list;
+    private Long total;
+}
