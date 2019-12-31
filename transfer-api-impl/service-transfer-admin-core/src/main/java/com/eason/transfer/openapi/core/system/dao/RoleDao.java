527ac48bc8cf4dd9097c6cbd63876920a876668d
ares123456 <ares123@qq.com>
2019-12-31 19:24:05 +0800

    用户管理添加配置

--- transfer-api-impl/service-transfer-admin-core/src/main/java/com/eason/transfer/openapi/core/system/dao/RoleDao.java
+++ transfer-api-impl/service-transfer-admin-core/src/main/java/com/eason/transfer/openapi/core/system/dao/RoleDao.java
@@ -2,7 +2,8 @@ package com.eason.transfer.openapi.core.system.dao;
 
 import com.eason.transfer.openapi.core.system.entity.po.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface RoleDao extends JpaRepository<Role, String> {
-
+    List<Role> findAllById(String id);
 }
