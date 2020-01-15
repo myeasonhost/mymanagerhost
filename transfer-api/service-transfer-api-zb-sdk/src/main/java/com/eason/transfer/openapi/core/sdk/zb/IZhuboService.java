26489d2c5cf3d4f3fcde8f888c89278ce13fe6e5
ares123456 <ares123@qq.com>
2020-01-15 15:40:19 +0800

    修改接口

--- transfer-api/service-transfer-api-zb-sdk/src/main/java/com/eason/transfer/openapi/core/sdk/zb/IZhuboService.java
+++ transfer-api/service-transfer-api-zb-sdk/src/main/java/com/eason/transfer/openapi/core/sdk/zb/IZhuboService.java
@@ -13,15 +13,78 @@ import org.springframework.web.bind.annotation.RequestBody;
  */
 @FeignClient(contextId = "zb#IZhuboService",value = "service-transfer-api-zb")
 public interface IZhuboService {
-
+    /**
+     * @apiVersion 1.0.0
+     * @apiGroup zhubo
+     * @api {POST} /zhubo/start 直播开播
+     * @apiName start
+     *
+     *
+     * @apiDescription
+     * * @apiSuccessExample Success-Response:
+     *      *      *   HTTP/1.1 200 OK
+     *
+     *
+     * {
+     *   "ZhuboStartResponse": {
+     *     "successCount": "1",
+     *     "errorCount": "0",
+     *     "result": "直播开播成功"
+     *   }
+     * }
+     *
+     */
     @CrossOrigin(origins = "*")
     @PostMapping(value = "/zhubo/start")
     ZhuboStartResponse start(@RequestBody ZhuboStartRequest request) throws Exception;
 
+    /**
+     * @apiVersion 1.0.0
+     * @apiGroup zhubo
+     * @api {POST} /zhubo/stop 直播开播
+     * @apiName stop
+     *
+     *
+     * @apiDescription
+     * * @apiSuccessExample Success-Response:
+     *      *      *   HTTP/1.1 200 OK
+     *
+     *
+     *{
+     *   "ZhuboStopResponse": {
+     *     "successCount": "1",
+     *     "errorCount": "0",
+     *     "result": "直播间销毁成功"
+     *   }
+     * }
+     *
+     */
     @CrossOrigin(origins = "*")
     @PostMapping(value = "/zhubo/stop")
     ZhuboStopResponse stop(@RequestBody ZhuboStopRequest request) throws Exception;
 
+
+    /**
+     * @apiVersion 1.0.0
+     * @apiGroup zhubo
+     * @api {POST} /zhubo/create 创建直播
+     * @apiName create
+     *
+     *
+     * @apiDescription
+     * * @apiSuccessExample Success-Response:
+     *      *      *   HTTP/1.1 200 OK
+     *
+     *
+     * {
+     *   "ZhuboCreateResponse": {
+     *     "successCount": "1",
+     *     "errorCount": "0",
+     *     "result": "主播信息更新成功"
+     *   }
+     * }
+     *
+     */
     @CrossOrigin(origins = "*")
     @PostMapping(value = "/zhubo/create")
     ZhuboCreateResponse create(@RequestBody ZhuboCreateRequest request) throws Exception;
